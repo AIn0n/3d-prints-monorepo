6e2a70796a5c7d3b11c235c43ce0363603c94215
AIn0n <55995692+AIn0n@users.noreply.github.com>
2026-08-19 22:27:28 +0200

    Minor fix at connector generation

--- midi_keyboard/connectors.py
+++ midi_keyboard/connectors.py
@@ -4,7 +4,14 @@ from solid2 import square
 from solid2.extensions.bosl2 import trapezoid
 
 
-def generate_female_connector(width: float, len_: float, conf: ConfigSchema):
+def normalize_width_len_connector(width: float, len_: float) -> tuple[float, float]:
+    return (width, len_) if len_ >= width * 2 else (len_ // 2, len_)
+
+
+def generate_female_connector(
+    width: float, len_: float, conf: ConfigSchema, downscale: bool = True
+):
+    width, len_ = normalize_width_len_connector(width, len_)
     conn_height = min(width, conf.base_height_mm)
     w2 = width + conf.connector_dims.base_diff_mm
 
@@ -21,13 +28,13 @@ def generate_female_connector(width: float, len_: float, conf: ConfigSchema):
     conn = square([width, len_]).linear_extrude(conn_height)
 
     offset = width * 2
-    conn_n = len_ // offset
+    conn_n = int(len_ // offset)
     adjusted_offset = len_ / conn_n
 
     for i in range(conn_n):
         conn -= female_conn.translateY(adjusted_offset / 2 + i * adjusted_offset)
 
-    return conn
+    return conn.down(width) if downscale else conn
 
 
 def generate_male_connector(width: float, len_: float, conf: ConfigSchema):
@@ -49,7 +56,7 @@ def generate_male_connector(width: float, len_: float, conf: ConfigSchema):
     conn = square([width, len_]).linear_extrude(conn_height)
 
     offset = width * 2
-    conn_n = len_ // offset
+    conn_n = int(len_ // offset)
     adjusted_offset = len_ / conn_n
 
     for i in range(conn_n):
