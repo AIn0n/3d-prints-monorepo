48fc8c592ed3be74fdf3f96f8e4a569815ae51cd
AIn0n <55995692+AIn0n@users.noreply.github.com>
2026-08-19 21:26:08 +0200

    Added both types connectors

---
+++ midi_keyboard/connectors.py
@@ -0,0 +1,58 @@
+from configuration import ConfigSchema
+
+from solid2 import square
+from solid2.extensions.bosl2 import trapezoid
+
+
+def generate_female_connector(width: float, len_: float, conf: ConfigSchema):
+    conn_height = min(width, conf.base_height_mm)
+    w2 = width + conf.connector_dims.base_diff_mm
+
+    female_conn = (
+        trapezoid(
+            h=width,
+            w1=width,
+            w2=w2,
+        )
+        .linear_extrude(conn_height)
+        .rotateZ(-90)
+        .translateX(width * 0.5)
+    )
+    conn = square([width, len_]).linear_extrude(conn_height)
+
+    offset = width * 2
+    conn_n = len_ // offset
+    adjusted_offset = len_ / conn_n
+
+    for i in range(conn_n):
+        conn -= female_conn.translateY(adjusted_offset / 2 + i * adjusted_offset)
+
+    return conn
+
+
+def generate_male_connector(width: float, len_: float, conf: ConfigSchema):
+    margin = conf.connector_dims.margin_mm
+
+    conn_height = min(width, conf.base_height_mm)
+    w2 = width + conf.connector_dims.base_diff_mm - margin
+
+    male_conn = (
+        trapezoid(
+            h=width - margin,
+            w1=width - margin,
+            w2=w2,
+        )
+        .linear_extrude(conn_height - margin)
+        .rotateZ(-90)
+        .translateX(width + (width - margin) * 0.5)
+    )
+    conn = square([width, len_]).linear_extrude(conn_height)
+
+    offset = width * 2
+    conn_n = len_ // offset
+    adjusted_offset = len_ / conn_n
+
+    for i in range(conn_n):
+        conn += male_conn.translateY(adjusted_offset / 2 + i * adjusted_offset)
+
+    return conn
