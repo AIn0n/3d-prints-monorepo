de2a7495b2d1aff33c9b250a83bce409816f8007
AIn0n <55995692+AIn0n@users.noreply.github.com>
2026-08-19 23:32:27 +0200

    Moved key generation to other file

--- midi_keyboard/common.py
+++ midi_keyboard/common.py
@@ -1,5 +1,4 @@
-from solid2 import square, circle, polygon, cube, cylinder
-from solid2.extensions.bosl2 import round_corners
+from solid2 import square, cube, cylinder
 from itertools import accumulate
 
 from configuration import ConfigSchema
@@ -86,29 +85,6 @@ def generate_octave(white_keys: int, conf: ConfigSchema):
             -conf.white_black_keys_offset_mm - conf.mount_plate_width,
         ]
     )
-
-
-def generate_mx_stem():
-    mx_stem_l = 4.0
-    mx_stem_w = 1.2
-    mx_stem_h = 3.0
-
-    mx_stem_arm = square([mx_stem_l, mx_stem_w], center=True)
-    mx_stem_base = circle(d=5.5) - (mx_stem_arm + mx_stem_arm.rotateZ(90))
-
-    return mx_stem_base.linear_extrude(height=mx_stem_h)
-
-
-def generate_key(width: float, length: float, conf: ConfigSchema):
-    stem = generate_mx_stem()
-    u = conf.keycap_u
-    h = conf.keycap_height_mm
-
-    keycap_base = square([u * width, u * length], center=True)
-    rounded_keycap = polygon(
-        round_corners(path=keycap_base, radius=conf.keycap_rounding_corner_mm)
-    ).linear_extrude(height=h)
-    return rounded_keycap + stem.up(h - 0.01)
 
 
 def generate_keys_row(
---
+++ midi_keyboard/mx_keys.py
@@ -0,0 +1,27 @@
+from solid2 import square, circle, polygon
+from solid2.extensions.bosl2 import round_corners
+
+from configuration import ConfigSchema
+
+
+def generate_mx_stem():
+    mx_stem_l = 4.0
+    mx_stem_w = 1.2
+    mx_stem_h = 3.0
+
+    mx_stem_arm = square([mx_stem_l, mx_stem_w], center=True)
+    mx_stem_base = circle(d=5.5) - (mx_stem_arm + mx_stem_arm.rotateZ(90))
+
+    return mx_stem_base.linear_extrude(height=mx_stem_h)
+
+
+def generate_key(width: float, length: float, conf: ConfigSchema):
+    stem = generate_mx_stem()
+    u = conf.keycap_u
+    h = conf.keycap_height_mm
+
+    keycap_base = square([u * width, u * length], center=True)
+    rounded_keycap = polygon(
+        round_corners(path=keycap_base, radius=conf.keycap_rounding_corner_mm)
+    ).linear_extrude(height=h)
+    return rounded_keycap + stem.up(h - 0.01)
