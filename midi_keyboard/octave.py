77be47a507043a7f303758e62a9435643407452f
AIn0n <55995692+AIn0n@users.noreply.github.com>
2026-09-12 23:18:12 +0200

    fully refactored

--- midi_keyboard/octave.py
+++ midi_keyboard/octave.py
@@ -105,7 +105,7 @@ class OctaveBlackPartBuilder(GroupBuilder):
             conf,
         )
         self.middle_wall = CubeBuilder(octave_width, conf.mount_plate_width, bw_diff)
-        # self.outer_arc = ArcBuilder(octave_width, bw_diff)
+        self.outer_arc = ArcBuilder(octave_width, bw_diff)
         self.back_wall = CubeBuilder(
             octave_width, conf.mount_plate_width, bw_diff + conf.base_height_mm
         )
@@ -124,11 +124,11 @@ class OctaveBlackPartBuilder(GroupBuilder):
             self.key_row, RelativeCoords(ypos=YPos.BACK), RelativeCoords(zpos=ZPos.TOP)
         )
         self.middle_wall.move_rel(self.key_row, RelativeCoords(zpos=ZPos.BOTTOM))
-        # self.outer_arc.move_rel(
-        #     self.middle_wall,
-        #     RelativeCoords(ypos=YPos.FRONT),
-        #     RelativeCoords(zpos=ZPos.BOTTOM),
-        # )
+        self.outer_arc.move_rel(
+            self.key_row,
+            RelativeCoords(ypos=YPos.FRONT),
+            RelativeCoords(zpos=ZPos.TOP),
+        )
         self.female_connector.move_rel(self.key_row, RelativeCoords(zpos=ZPos.BOTTOM))
         self.male_connector.move_rel(
             self.key_row,
@@ -157,60 +157,24 @@ class OctaveBuilder(GroupBuilder):
         self.white_part = OctaveWhitePartBuilder(white_keys, conf)
 
         self.white_part.move_rel(
-            self.black_part.middle_wall,
-            RelativeCoords(ypos=YPos.FRONT),
-            # RelativeCoords(ypos=YPos.BACK),
+            self.black_part.outer_arc,
+            RelativeCoords(zpos=ZPos.BOTTOM),
+            RelativeCoords(ypos=YPos.BACK),
+        )
+        self.male_connectors_slope = SlopeBuilder(
+            self.white_part.male_connector.dx,
+            self.black_part.key_row.dy,
+            self.black_part.key_row.z - self.white_part.male_connector.z,
+        )
+        self.male_connectors_slope.move_rel(
+            self.black_part.key_row,
+            RelativeCoords(zpos=ZPos.BOTTOM),
+            RelativeCoords(xpos=XPos.RIGHT),
         )
 
     def build(self) -> Any:
-        return self.black_part.build() + self.white_part.build()
-
-
-# def generate_octave(white_keys: int, conf: ConfigSchema):
-
-#     assert bw_diff <= (conf.white_key_dims.key_offset_y(conf) - 1), (
-#         "Distance between white and black keys is too high, cannot generate arc between them"
-#     )
-
-#     white_part, wp_male_connector_width, wp_male_connector_height = (
-#         generate_kb_white_key_part(octave_width, white_keys, conf)
-#     )
-
-#     b_distances = get_black_key_dist(wk_total_width, conf.mount_u)
-#     black_mount_plate = (
-#         # middle wall, between black and white keys
-#         +cube([octave_width, conf.mount_plate_width, bw_diff]).down(bw_diff)
-#         # middle wall outer arc, to make connection between white and black keys part stronger
-#         + arc(bw_diff, octave_width).up(conf.mount_plate_width)
-#         # Back wall of the keyboard
-#         + cube([octave_width, conf.mount_plate_width, bw_diff + conf.base_height_mm])
-#         .down(bw_diff + conf.base_height_mm)
-#         .translateY(conf.dist_u - conf.mount_plate_width)
-#         + generate_female_connector(b_distances[0], conf.dist_u, conf)
-#         + generate_male_connector(b_distances[0], conf.dist_u, conf).translateX(
-#             octave_width
-#         )
-#         # slope between black and white part of the keyboard connector
-#         + slope(
-#             wp_male_connector_width, conf.dist_u, bw_diff + wp_male_connector_height
-#         ).translate(
-#             [
-#                 octave_width - wp_male_connector_width,
-#                 0,
-#                 -(bw_diff + wp_male_connector_height),
-#             ]
-#         )
-#     )
-#     conn_width, _ = normalize_width_len_connector(b_distances[0], conf.dist_u)
-#     mid_wall_inner_slope = slope(
-#         octave_width - conn_width, bw_diff, bw_diff
-#     ).translateX(conn_width)
-#     key_hole = square([conf.mount_u, bw_diff]).linear_extrude(bw_diff)
-#     for dist in accumulate(b_distances):
-#         mid_wall_inner_slope -= key_hole.translateX(dist)
-
-#     return (
-#         black_mount_plate
-#         + mid_wall_inner_slope.translateY(conf.mount_plate_width).down(bw_diff)
-#         + white_part
-#     )
+        first, *rest = self.movable_parts
+        model = first.build()
+        for part in rest:
+            model += part.build()
+        return model
