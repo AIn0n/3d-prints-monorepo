8d898c9a76ee5a3461c53ac464fdb86cad8059b0
AIn0n <55995692+AIn0n@users.noreply.github.com>
2026-09-21 21:07:51 +0200

    Added backplate generator

---
+++ midi_keyboard/backplate.py
@@ -0,0 +1,25 @@
+from base_builders import *
+from configuration import ConfigSchema
+from common import CubeBuilder, StandBuilder
+from solid2 import cylinder
+
+
+class BackplateBuilder(GroupBuilder):
+    def __init__(self, model: GroupBuilder, conf: ConfigSchema) -> None:
+        self.c = CubeBuilder(model.dx, model.dy, conf.backplate_dims.width_mm)
+        self.c.move_rel(
+            model,
+            RelativeCoords(zpos=ZPos.BOTTOM),
+            RelativeCoords(ypos=YPos.FRONT, xpos=XPos.LEFT),
+        )
+        self.stands_cords = [
+            (el.x, el.y) for el in model.movable_parts if isinstance(el, StandBuilder)
+        ]
+        self.hole_r = conf.backplate_dims.screw_r_mm
+        super().__init__()
+
+    def build(self):
+        model = self.c.build()
+        for x, y in self.stands_cords:
+            model -= cylinder(h=self.dz, r=self.hole_r).translate([x, y, self.z])
+        return model
